72f82ca910ebfbb9b023fbe6243b954148a5b2c3
Behrooz Vedadian <vedadian@gmail.com>
2025-05-10 15:35:22 +0330

    fix: Resolve bugs in implementation of previous feature

--- postbuild.bash
+++ postbuild.bash
@@ -2,7 +2,7 @@
 
 cd $(dirname $0)
 if cd ./dist; then
-  find . -name "*.tsx" -exec fgrep "<Route " {} \; | perl -pe 's%.*<Route path="(.*?)/?".*%\1%g' | grep -v "^$" | while read P; do
+  find ../src -name "*.tsx" -exec fgrep "<Route " {} \; | perl -pe 's%.*<Route path="(.*?)/?".*%\1%g' | grep -v "^$" | while read P; do
     echo "Creatubg structure for $P ..."
     INDEX_HTML=$(printf '../%.0s' $(seq 1 $(grep -o "/" <<<"$P" | wc -l)))index.html
     if mkdir -p ".$P" && ln -snf $INDEX_HTML ".$P/"; then

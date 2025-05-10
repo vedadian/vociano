e400d6251788c5228d59ab39d7a1dcd45c30acff
Behrooz Vedadian <vedadian@gmail.com>
2025-05-10 12:22:59 +0330

    feat: Add tailwindcss and github flavoured markdown

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Simple Vocal Analyser
 
 This project aims to be both a demonstration for my skills and a utility for me and my son (maybe others 😄). I will update the `README.md` file while developing the project.
+
+```ts
+const x: number = 12;
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehypeSanitize from "rehype-sanitize";
+import readmeMd from "../README.md?raw";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="fixed left-0 top-0 w-full h-full flex flex-col justify-center items-center">
+      <div className="markdown-body w-[1024px]">
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeSanitize]}
+      >
+        {readmeMd}
+      </ReactMarkdown>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;

63e7ec84ab9615026fa933475f057e2dc3c72e2c
Behrooz Vedadian <vedadian@gmail.com>
2025-05-10 15:21:05 +0330

    fix: Change naked react-router link to radix themed link

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+import { Link as RadixLink } from "@radix-ui/themes";
 import { Link } from "react-router";
 
 export function Main() {
   return (
     <>
       <h1>Hello Dear Singer!</h1>
-      <Link to={"/about"}>About</Link>
+      <RadixLink asChild>
+        <Link to={"/about"}>About</Link>
+      </RadixLink>
     </>
   );
 }
